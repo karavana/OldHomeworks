1af12571cdda44fc393609a4dc6d01b984257ead
Oguz Xo <karavanasam_03@hotmail.com>
2016-05-03 16:41:37 +0300

    Create app.js
    This is to demonstrate clustering in node.js

---
+++ app.js
@@ -0,0 +1,39 @@
+// Include the cluster module
+var cluster = require('cluster');
+
+// Code to run if we're in the master process
+if(cluster.isMaster){
+	 // Count the machine's CPUs
+    var cpuCount = require('os').cpus().length;
+	// Create a worker for each CPU
+    for (var i = 0; i < cpuCount; i += 1) {
+        cluster.fork();
+    }
+	
+	// Listen for dying workers
+    cluster.on('exit', function (worker) {
+
+        // Replace the dead worker, we're not sentimental lol
+        console.log('Worker %d died :(', worker.id);
+        cluster.fork();
+
+    });
+	
+	
+}
+else{
+	// Include Express
+	var express = require('express');
+
+	// Create a new Express application
+	var app = express();
+
+	// Add a basic route – index page
+	app.get('/', function (req, res) {
+		res.send('Hello from Worker ' + cluster.worker.process.pid);
+	});
+
+	// Bind to a port
+	app.listen(3000);
+	console.log('Worker %d running!', cluster.worker.process.pid);
+}
